3bdcaaef483269ade514e16e3581a0d3db020577
charnet1019 <charnet1019@163.com>
2020-07-30 19:56:11 +0800

    Create setenv.sh

---
+++ etcdctl/setenv.sh
@@ -0,0 +1,5 @@
+export ETCDCTL_API=3
+export ETCDCTL_ENDPOINTS="https://192.168.2.10:2379"
+export ETCDCTL_CACERT="/etc/kubernetes/pki/etcd/ca.crt"
+export ETCDCTL_CERT="/etc/kubernetes/pki/etcd/server.crt"
+export ETCDCTL_KEY="/etc/kubernetes/pki/etcd/server.key"
